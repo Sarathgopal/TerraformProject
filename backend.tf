a870691aa82783995e085ba7321b6cb3f3667ff6
Sarathgopal <43115968+Sarathgopal@users.noreply.github.com>
2024-12-27 17:37:10 +0100

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "terraform-backend-rg"
-    storage_account_name = "terraformstorageebi"
+    storage_account_name = "terraformstorageeib"
     container_name       = "tfstate"
     key                  = "${terraform.workspace}/terraform.tfstate"
   }
