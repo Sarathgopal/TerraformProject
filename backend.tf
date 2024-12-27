acb8263e1fa503cffbe34eac2aa32741bb569287
Sarathgopal <43115968+Sarathgopal@users.noreply.github.com>
2024-12-27 17:36:31 +0100

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,6 +3,6 @@ terraform {
     resource_group_name  = "terraform-backend-rg"
     storage_account_name = "terraformstorageebi"
     container_name       = "tfstate"
-    key                  = "<env>/terraform.tfstate"
+    key                  = "${terraform.workspace}/terraform.tfstate"
   }
 }
